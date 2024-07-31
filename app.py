36519901a599eff187ca8aed7f88038b751340c2
r4xon <r4xon@Linux.myguest.virtualbox.org>
2024-07-31 10:17:50 +0200

    s

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello world</h1>"
